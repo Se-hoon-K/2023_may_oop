73e0ca1456c7c5aa81146f1d068bf962a13caf86
Se-hoon-K <133936213+Se-hoon-K@users.noreply.github.com>
2023-05-23 17:15:40 +0900

    Add files via upload

---
+++ week2/MiniProject1.java
@@ -0,0 +1,12 @@
+package week2;
+
+public class MiniProject1 {
+
+	public static void main(String[] args) {
+		System.out.println(10+20);
+		System.out.println(20-10);
+		System.out.println(10*20);
+		System.out.println(20/10);
+	}
+
+}
